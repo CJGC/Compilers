fdb6d8997a5986849b82c2bf0f7e327e74b62617
Christhian Gomez <chris093.013@gmail.com>
2016-11-14 16:18:47 -0500

    Was added test2.go file

---
+++ gosemantic_files/test2.go
@@ -0,0 +1,15 @@
+func funcion(x flt, y bool) float {
+  if true >= true {
+    print "Es true";
+    return 0,1;
+  }
+  else{
+    return 0;
+  }
+
+  if true + true {
+    a = "valor" - "valor";
+    a = "valor" + "valor";
+  }
+  return 0.3;
+}
