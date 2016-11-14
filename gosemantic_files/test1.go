f6203418c0f327cbb9f445e5c48fb34d4f65024e
Christhian Gomez <chris093.013@gmail.com>
2016-11-14 16:14:27 -0500

    Were performed some changes on test files.go

--- gosemantic_files/test1.go
+++ gosemantic_files/test1.go
@@ -1,8 +1,25 @@
 extern func sin(x float,y float) int;
 
 const e = 3.0;
+const e = 2.0;
 
 var t bool = true;
+var t bool = false;
+
+extern func sin(z float,w float) int;
+extern func cos(q float,r float) floa;
+
+func primo(x float, y bool) flt {
+  print "malo";
+  return 0.0;
+}
+
+return 3;
+
+func sintipo(x float, t float) {
+  return 0;
+}
+
 if -t < false{
   print "verdadero";
 }
--- gosemantic_files/test5.go
+++ gosemantic_files/test5.go
@@ -1,21 +1,22 @@
 var a int = 3;
 var b float = 5.3;
 const c = 3.0;
-func suma(x float, y float)  float{
+func suma(x int, y float)  int{
 	//return x+y;
 	print ":D";
 	return 3.0;
 }
-const f = suma(2.0,3.0);
-var v float = suma(2.0,suma(3.0,3.3));
-func funcion(x float, y float) int {
+const c = suma(2.0,3.0);
+var v float = suma(2.0,c(3.0,3.3));
+
+func funcion(x float, y bool) float {
 	print (x+y);
 	print b+x;
 	x = 7.0;
 	if x > b {
 		print x;
 		print "x es mayor";
-		x = b+2.0;
+		x = (2+2.0);
 
 		while x >= b {
 			print x;
@@ -27,13 +28,13 @@ func funcion(x float, y float) int {
 		print x;
 		print "x es menor";
 	}
-	//return funcion(funcion(2.0,funcion(1.2,2.3)),funcion(3.3,23.23));
+	return funcion(funcion(2.0,funcion(1.2,2.3)),funcion(3.3,23.23));
 	while x >= b {
 		print x;
 		x = x-1.0;
 		print a;
 	}
-	//return (3.2+ suma(3.0,32.3));
+	return (3.2+ suma(3.0,32.3));
 	a = funcion(3.2,3.0);
 	print funcion(2.3,(23.0*3.0));
 	print (suma(3.0,suma(3.3,4.0)))+3.0;
