9a4c3643f947cdde042a413038e77d85c90fd310
Christhian <chjugomez@utp.edu.co>
2016-11-25 01:04:56 -0500

    More files.go testing

---
+++ gosemantic_files/test3.go
@@ -0,0 +1,30 @@
+if true {
+}
+else{
+}
+
+while 5>3{
+}
+
+func primo(x float, y bool) bool {
+  if true{
+    if true{
+      return 3;
+    }
+    else{
+      return 3;
+      if true{
+        return 3;
+        while true{
+          return 3;
+          if true{
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
+return "Genius..";
---
+++ gosemantic_files/test4.go
@@ -0,0 +1,29 @@
+func val(x int,y int) float {
+  return 3;
+}
+const z = val(3,4);
+var x float;
+var valor float = -(30.3 + val(3,3));
+print "hello" + "hello";
+print "hello" - "hello";
+var valor float = 5 < 5;
+const valor = val(3,2);
+print valor;
+valor =  valor < valor;
+
+if false {
+  x = valor;
+}
+
+while true {
+  x = valor;
+  print valor;
+  return valor;
+}
+return 30;
+print val(33,3);
+extern func sin() int;
+extern func cos(x float,a float) float;
+cos(3.33,2.9);
+sin();
+extern func sin(t float,w float) int;
