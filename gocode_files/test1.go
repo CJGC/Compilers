309c1a2f495e1e9f512fa7e04c1b51216c47cca2
Christhian Gomez <chjugomez@utp.edu.co>
2016-12-02 18:14:20 -0500

    Test files.go belonging to gocode.py module

---
+++ gocode_files/test1.go
@@ -0,0 +1,25 @@
+// extern func sin(x float,y float) int;
+//
+while true{
+  if true{
+    var e float;
+    //e = -3.0+3.4;
+    e = +5.0-3.0;
+    while e >= 0.0 {
+      print e;
+    }
+  }
+  else {
+    const t = 3.0;
+  }
+}
+//
+// var t float;
+// var w float;
+// t = t / w;
+// print t+w;
+
+// func primo(x float, y bool) float {
+//   print t;
+//   return -0.0;
+// }
