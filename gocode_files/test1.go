c4cf604b5934320283b711f01e2dc0b639fc54d7
Christhian Gomez <chjugomez@utp.edu.co>
2016-12-12 19:45:37 -0500

    More test cases were considered

--- gocode_files/test1.go
+++ gocode_files/test1.go
@@ -1,18 +1,24 @@
 // extern func sin(x float,y float) int;
-//
-while true{
-  if true{
-    var e float;
-    //e = -3.0+3.4;
-    e = +5.0-3.0;
-    while e >= 0.0 {
-      print e;
-    }
-  }
-  else {
-    const t = 3.0;
-  }
+func sin(x float,y float) float {
+  var value float = 3.0;
+  return value;
 }
+
+// while true{
+//   if true{
+    // var e float;
+    //e = -3.0+3.4;
+    // sin(3.0,2.3);
+    // e = -(sin(2.3,4.3) + sin(21.3,sin(0.0,0.0)));
+    // print ("hello");
+//     while e >= 0.0 {
+//       print e;
+//     }
+//   }
+//   else {
+//     const t = 3.0;
+//   }
+// }
 //
 // var t float;
 // var w float;
